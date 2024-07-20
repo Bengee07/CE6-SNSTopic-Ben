0d6916673fc54740a63ed74b2757fa0c9a896fe0
Bengee07 <bengeeepaton@gmail.com>
2024-07-20 11:37:35 +0800

    New test again

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "BenSNSTopic" {
-  type = string
-  default = "lol"
-  description = "This is a variable"
+  type        = string
+  default     = "dance"
+  description = "This is a variable2"
 }
 variable "ben" {
   type        = string
