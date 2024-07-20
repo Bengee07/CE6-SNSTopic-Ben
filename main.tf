f28dea1fd7bc576c571acc4c5296ecfd8d595b49
Bengee07 <bengeeepaton@gmail.com>
2024-07-20 11:39:55 +0800

    New Test 10

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-locals {
-  Name = "BenSNSTopic"
-}
-
 resource "aws_sns_topic" "user_updates" {
   name = var.ben
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "BenSNSTopic" {
-  type        = string
-  default     = "dance"
-  description = "This is a variable2"
-}
 variable "ben" {
   type        = string
   default     = "lol"
