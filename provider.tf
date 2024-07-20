206bbfbdc245839adde09f828102148848f3c0c3
Bengee07 <bengeeepaton@gmail.com>
2024-07-20 11:43:46 +0800

    newtest9

--- provider.tf
+++ provider.tf
@@ -1,3 +1,20 @@
+terraform {
+
+  required_providers {
+
+    aws = {
+
+      source = "hashicorp/aws"
+
+      version = "3.0"
+    }
+
+  }
+
+}
+
 provider "aws" {
+
   region = "ap-southeast-1"
+
 }
